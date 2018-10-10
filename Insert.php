c18a1375635864bacfc76f7f02f9f2f8ce5caaa7
chaowalee <42596892+chaowalee@users.noreply.github.com>
2018-10-10 09:42:30 +0700

    Add files via upload

--- Insert.php
+++ Insert.php
@@ -2,20 +2,35 @@
 	<head>
 	<title>PQ PEAS1</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0," charset="utf-8">
-		<title>Data Record</title>
+		<title>PQ PEAS1</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
-		
+		<style type="text/css">
+			.row-center
+			{
+				text-align:center;<!การสั่งให้ตัวแปร row-center มีข้อความข้างในอยู่กลางคอลัมน์>
+			}
+		</style>
 	</head>
 		<body>
-			<div class ="container-fluid" style="background-color:purple;">
-				<div class="row row-center">
-					<div class="col-lg-4 offset-lg-4" style="background-color:pink;">
-						<h4>บันทึกข้อมูล</h4>
+			<div class ="container-fluid" style="background-color:purple;"><!container มันคือพารากราฟใน 1 กล่อง สามารถแบ่งแถวและคอลัมน์ได้>
+				<div class="row row-center"><!เป็นคำสั่งของแถวแต่ละแถว ถ้าอยากเพิ่มแถวใช้บรรทัดที่ 16-18>
+					<div class="col-lg-4 offset-lg-4" style="background-color:pink;"><!เป็นการแบ่งคอลัมน์ในแต่ละแถว>
+						<h4>Data Record</h4>
+					</div>
+				</div>
+			</div>
+			<div class ="container">
+				<div class="row">
+					<div class="col-lg-2" style="background-color:yellow;">
+						<h4>Coll</h4><!h4 คือขนาดตัวอักษร ใหญ่ เล็ก มี h1-6 1 ใหญ่สุด>
+					</div>
+					<div class="col-lg-10" style="background-color:blue;">
+						<h4>Col2</h4>
 					</div>
 				</div>
 			</div>
