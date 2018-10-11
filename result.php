15274224479d2a2b345ce2cf7a40fb5d87c88ee7
chaowalee <42596892+chaowalee@users.noreply.github.com>
2018-10-11 09:36:04 +0700

    Add files via upload

--- result.php
+++ result.php
@@ -5,6 +5,7 @@
 <title>PQ PEAS1</title>
 <!-- css -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Mali" rel="stylesheet">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link href="tagsinput.css" rel="stylesheet" type="text/css">
 <script src="tagsinput.js"></script>
@@ -13,6 +14,16 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
 <style type="text/css">
+   .row-center
+   {
+    text-align:center;
+   }
+   body 
+   {
+    font-family: 'Mali', cursive;
+   }
+ </style>
+
 #container-fluid{
 margin-bottom: 10px;
 }
