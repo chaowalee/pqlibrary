dbf0ef16396003bca84f99f1c55db96233782b75
chaowalee <42596892+chaowalee@users.noreply.github.com>
2018-10-12 21:57:58 +0700

    Add files via upload

--- render_msg.php
+++ render_msg.php
@@ -26,7 +26,7 @@ function flex_msg($keyword)
 	}
 	else if ($numrows < 1)
 	{
-		$url = "https://nutt-i.com/psqv2";
+		$url = "0863873370";
 		$txtresult = "0 item";
 		$btn_txt = "ติดต่อผู้ดูแล";
 	}
