9c841033f1da88200b9647494e1352b5c378f146
chaowalee <42596892+chaowalee@users.noreply.github.com>
2018-10-12 07:37:12 +0700

    Add files via upload

--- pdf.php
+++ pdf.php
@@ -23,7 +23,7 @@
 	</head>
 	<body>
 		<?php
-		$link = trim($_GET["doc_no"]);
+		$link = trim($_GET["docpdf"]);
 		$server = "us-cdbr-iron-east-01.cleardb.net";
 		$username = "b21e535520af4b";
 		$password = "402bbf1f";
