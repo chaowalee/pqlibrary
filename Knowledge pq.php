fadaa4bef5e63f066dcb49aa913e3ff6270fc8ad
chaowalee <42596892+chaowalee@users.noreply.github.com>
2018-12-12 15:39:09 +0700

    Add files via upload

--- Knowledge pq.php
+++ Knowledge pq.php
@@ -59,9 +59,9 @@ function reply_flex_msg($keyword,$replyToken)
 			{
 			$replyToken = $event['replyToken']; //ประกาศตัวแปร replyToken เก็บ reply token ที่ไลน์ event ส่งมาในทุกครั้งอะ เอาไว้ใสตัวแปรที่ประกาศ เพื่อเอาไว้ให้ตอบกลับ
             $txtin = $event['message']['text'];//เอาข้อความจากไลน์ที่ผ่านการตรวจสอบบรรทัดที่ 29 มาเก็บในตัวแปร $txtin
-			if($txtin == "Keyword")//strtolower หมายถึงตัวแปรที่เข้ามาแปลงเปนตัวเล็กหมด 
+			if(strtolower($txtin) == "keyword")//strtolower หมายถึงตัวแปรที่เข้ามาแปลงเปนตัวเล็กหมด 
 			{
-				$sql_text = "SELECT * FROM librarypq";//select* หมายถึงเลือกทั้งตาราง 
+				$sql_text = "SELECT DISTINCT keyword FROM librarypq";//select* หมายถึงเลือกทั้งตาราง 
 				$query = mysqli_query($conn,$sql_text);// ให้หาข้อมูลที่เก็บไว้ในตัวแปร $con และ $sql_text
 				while($obj = mysqli_fetch_array($query))//เป็นการคลี่ข้อมูลออกมาที่ละบรรทัดแล้วไปใส่ในตัวแปร obj
 				{
